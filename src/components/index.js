43c1875bb4764285ac93e647b204625db22e07c7
slizer98 <erick.m.villa.r@gmail.com>
2023-07-03 12:28:25 -0600

    separado html en componentes | agregando funcionalidad al carrito(incompleto)

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+
+// Archivo barril Vue
+
+import Header from './Header.vue';
+import Guitarra from './Guitarra.vue';
+import Footer from './Footer.vue';
+
+export {
+    Header,
+    Guitarra,
+    Footer
+}
